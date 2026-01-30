0df45926b64e2d90c0cb63d6e1402d5a0768d2bd
JJLOLLOL <jiajing2005@gmail.com>
2026-01-30 11:34:54 +0800

    Create Mock_Layout.java

---
+++ src/Mock_Layout.java
@@ -0,0 +1,6 @@
+public class Mock_Layout {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test output");
+    }
+}
