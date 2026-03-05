57d54a89952dc4f250d0ddb39c51d854a0ceb235
Galaxius03 <dj.liverpool18@gmail.com>
2026-03-05 21:40:50 +0800

    updated career and skills classes, added progressBar interface to be used by both

--- src/models/Career.java
+++ src/models/Career.java
@@ -1,16 +1,18 @@
 package models;
 
-public class Career {
+public class Career implements ProgressBar{
     public  String title;
     public double salary;
-    public double duration;
-    public int progression;
+    public double duration; //in hours to represent duration the Sim is at work
+    public double progress;
+    public int level;
 
     public Career(String title, double salary, double duration) {
         this.title = title;
         this.salary = salary;
+        this.level = 1;
         this.duration = duration;
-        this.progression = 0;
+        this.progress = 0.0;
     }
 
     public String getTitle() {
@@ -21,8 +23,18 @@ public class Career {
         return salary;
     }
 
-    public int getProgression() {
-        return progression;
+    @Override
+    public double getProgress() {
+        return progress;
+    }
+
+    @Override
+    public void addProgress(double amount){
+        this.progress += amount;
+        if (this.progress >= 100.0) {
+            this.level++;
+            this.progress -= 100.0;
+        }
     }
 
     public double getDuration() {
