b232743e3d313a1ad4ce9d14211653e225ae6d6c
Nicholas Bay <70332449+nicholas-bay@users.noreply.github.com>
2026-03-08 02:25:23 +0800

    feat: test trying seperating careerlist from career and implementing salaryMultipler for careerRank

--- src/models/SimCharacter.java
+++ src/models/SimCharacter.java
@@ -9,7 +9,6 @@ public class SimCharacter extends Character {
     private House house;
     private Map<String, Need> needs = new HashMap<>();
     private HashMap<String, Skills> skills = new HashMap<>();
-    private HashMap<String, Career> careers = new HashMap<>();
     private Career career;
 
     public SimCharacter(String name, int age, String gender, Location defaultLocation, Career career) {
@@ -38,22 +37,6 @@ public class SimCharacter extends Character {
         skills.put("Music", new Skills("Music"));
         skills.put("Writing", new Skills("Writing"));
         skills.put("Painting", new Skills("Painting"));
-    }
-
-    public void initialiseCareers() {
-        careers.put("Software Developer", new Career("Software Developer", 4500.0, 8.0,  CareerTiers.INTERN));
-        careers.put("Engineer",           new Career("Engineer",           4200.0, 8.0,  CareerTiers.INTERN));
-        careers.put("Doctor",             new Career("Doctor",             6000.0, 10.0, CareerTiers.INTERN));
-        careers.put("Teacher",            new Career("Teacher",            3500.0, 8.0,  CareerTiers.INTERN));
-        careers.put("Lawyer",             new Career("Lawyer",             5500.0, 9.0,  CareerTiers.INTERN));
-        careers.put("Police Officer",     new Career("Police Officer",     3200.0, 10.0, CareerTiers.INTERN));
-        careers.put("Accountant",         new Career("Accountant",         3800.0, 8.0,  CareerTiers.INTERN));
-        careers.put("Business Manager",   new Career("Business Manager",   5000.0, 9.0,  CareerTiers.INTERN));
-        careers.put("Chef",               new Career("Chef",               3000.0, 10.0, CareerTiers.INTERN));
-        careers.put("Artist",             new Career("Artist",             2500.0, 6.0,  CareerTiers.INTERN));
-        careers.put("Musician",           new Career("Musician",           2600.0, 6.0,  CareerTiers.INTERN));
-        careers.put("Writer",             new Career("Writer",             2800.0, 7.0,  CareerTiers.INTERN));
-        careers.put("Jobless",            new Career("Jobless",            0.0,    0.0,  CareerTiers.INTERN));
     }
 
     // getters & setters
