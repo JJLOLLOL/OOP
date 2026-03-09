ddbcfed40aec1eb995a4b82613741f721179d850
itsjustmdawg <maheshauthayakumar33@gmail.com>
2026-03-10 04:23:53 +0800

    abstracted base state to display attributes and other status messages

--- src/core/GameEngine.java
+++ src/core/GameEngine.java
@@ -1,23 +1,66 @@
 package core;
 
+import models.Location;
+import models.SimCharacter;
+import ui.GameState;
+import ui.InitializationState;
+
 import java.util.Scanner;
-import states.CreationState;
 
 public class GameEngine {
+    private static GameEngine instance;
+    private SimCharacter activePlayer;
+    private Location currentLocation;
+    private boolean isRunning;
+    private GameState activeState;
+    private Scanner scanner;
 
-    private GameState currentState;
+    public GameEngine() {}
 
-    public GameEngine() {
-        currentState = new CreationState(this);
+    public static GameEngine getInstance() {
+        if (instance == null) {
+            instance = new GameEngine();
+        }
+        return instance;
+    }
+
+    public SimCharacter getActivePlayer(){
+        return activePlayer;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location newLocation) {
+        currentLocation = newLocation;
+    }
+
+    public void setActivePlayer(SimCharacter character) {
+        activePlayer = character;
+    }
+
+    public void setGameState(GameState newState) {
+        this.currentGameState = newState;
+    }
+
+    public void setIsRunning(boolean value) {
+        this.isRunning = value;
     }
 
     public void start() {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            currentState.render();
-            String input = scanner.nextLine();
-            currentState.handleInput(input);
-            currentState.update();
+        if (!isRunning) {
+            setIsRunning(true);
+            // Trigger point to start UI, navigating between interfaces handled by UI classes
+            setGameState(new InitializationState());
+            while (isRunning) {
+                activeState.render(this);
+
+                String input = scanner.nextLine().trim();
+                activeState.handleInput(input, this);
+            }
+        } else {
+            System.out.println("Game engine failed to start!");
         }
     }
 
