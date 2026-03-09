086eb72b443c5e8c32e52619ceb8312490af5068
Nicholas Bay <70332449+nicholas-bay@users.noreply.github.com>
2026-03-09 18:24:24 +0800

    feat: TODO for getters and setters

--- src/models/SimCharacter.java
+++ src/models/SimCharacter.java
@@ -1,22 +1,23 @@
 package models;
 
 import java.util.HashMap;
-import java.util.Map;
 import models.needs.*;
 
 public class SimCharacter extends Character {
     private boolean currentlyPlaying;
     private double money;
     private House house;
-    private Map<String, Need> needs = new HashMap<>();
-    private HashMap<String, Skills> skills = new HashMap<>();
     private Career career;
+    private HashMap<String, Need> needs = new HashMap<>();
+    private HashMap<String, Skills> skills = new HashMap<>();
 
     public SimCharacter(String name, int age, String gender, Location defaultLocation, Career career) {
         super(name, age, gender, defaultLocation);
         this.money = 1000.0;
         this.career = career;
+        this.currentlyPlaying = true;
         initialiseNeeds();
+        intialiseSkills();
     }
 
     private void initialiseNeeds() {
@@ -44,63 +45,36 @@ public class SimCharacter extends Character {
     public void setMoney(double amount) {
         money += amount;
     }
+
     public double getMoney() {
         return money;
-    private void initializeNeeds() {
-        // Core gameplay mechanics: Needs
-        registerNeed(new Hunger());
-        registerNeed(new Energy());
-        registerNeed(new Hygiene());
-        registerNeed(new Fun());
-        registerNeed(new Social());
     }
+    // TODO: implement getters and setters for skills and needs
 
-    private void registerNeed(Need need) {
-        needs.put(need.getNeedName(), need);
+    public void currentlyPlaying(boolean state) {
+        currentlyPlaying = state;
     }
 
     public void performActivity(Activity activity) {
-        System.out.println(name + " is doing " + activity.getName() + "...");
+        // System.out.println(name + " is doing " + activity.getName() + "...");
 
-        Map<String, Double> effects = activity.getNeedEffects();
-        for (Map.Entry<String, Double> entry : effects.entrySet()) {
-            String needName = entry.getKey();
-            Double amount = entry.getValue();
+        // Map<String, Double> effects = activity.getNeedEffects();
+        // for (Map.Entry<String, Double> entry : effects.entrySet()) {
+        //     String needName = entry.getKey();
+        //     Double amount = entry.getValue();
 
-            if (needName.equals("Money")) {
-                this.money += amount;
-            } else if (needs.containsKey(needName)) {
-                needs.get(needName).adjustNeed(amount);
-            }
-        }
-
-        if (activity.getName().equals("Work")) {
-            // Work improves Logic slightly?
-            // Skills.get("Logic").practice(10.0);
-        } else if (activity.getName().equals("Socialize")) {
-            // skills.get("Charisma").practice(15.0);
-        }
-    }
-
-    @Override
-    public void update(int minutesPassed) {
-    }
-    @Override
-    public void displayInfo() {
-        System.out.println("=== " + name + " (" + agegroup + ") ===");
-        System.out.println("Money: $" + String.format("%.2f", money));
-        // System.out.println("Career: " + career);
-        System.out.println("--- Needs ---");
-        for (Need need : needs.values()) {
-            System.out.println(need.toString());
-        }
-        // System.out.println("--- Skills ---");
-        // for (Skill skill : skills.values()) {
-        // System.out.println(skill.toString());
+        //     if (needName.equals("Money")) {
+        //         this.money += amount;
+        //     } else if (needs.containsKey(needName)) {
+        //         needs.get(needName).adjustNeed(amount);
+        //     }
         // }
-        System.out.println("==================");
-    }
 
-    public void performActivity(Activity activity) {
+        // if (activity.getName().equals("Work")) {
+        //     // Work improves Logic slightly?
+        //     // Skills.get("Logic").practice(10.0);
+        // } else if (activity.getName().equals("Socialize")) {
+        //     // skills.get("Charisma").practice(15.0);
+        // }
     }
 }
